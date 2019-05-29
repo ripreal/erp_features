48071d19eda8ebc8dfadaee774485d335d08c0b0
RKudakov <ripreal1@yandex.ru>
2019-05-29 21:07:19 +0300

    Промежуточный прогресс

--- copy_etalon/drop_db.bat
+++ copy_etalon/drop_db.bat
@@ -4,16 +4,34 @@ set server1c=%1
 set agentPort=%2
 set serverSql=%3
 set base=%4
-set admin1cUserLine=%5
-set sqluserLine=%6
-set sqlpasswLine=%7
-set fulldropLine=%8
+set admin1cUser=%5
+set admin1cPwd=%6
+set sqluser=%7
+set sqlpassw=%8
+set fulldrop=%9
 
+if "%sqluser%" == "" (
+    set sqluserLine=""
+)else (
+    set sqluserLine="-sqluser %sqluser%"
+)
 
-"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -file drop_db.ps1 -server1c %server1c% -agentPort %agentPort% -serverSql %serverSql% -infobase %base% %admin1cUserLine% %admin1cPwdLine% %sqluserLine% %sqlpasswLine% %fulldropLin%
+if "%sqlpassw%" == "" (
+    set sqlpasswLine=""
+)else (
+    set sqlpasswLine="-sqlPwd %sqlpassw%"
+)
+
+if "%fulldrop%" == "" (
+    set fulldropLine =""
+)else (
+    set fulldropLine ="-fulldrop true"
+)
+
+"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -file drop_db.ps1 -server1c %server1c% -agentPort %agentPort% -serverSql %serverSql% -infobase %base% %admin1cUser% %admin1cPwd% %sqluserLine% %sqlpassw% %fulldrop%
 if NOT %ERRORLEVEL% == 0 (
     echo "Script failed  when executing 64-bit powershell drop_db script. Let's try 32-bit..."
-    "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe" -file drop_db.ps1 -server1c %server1c% -agentPort %agentPort% -serverSql %serverSql% -infobase %base% %admin1cUserLine% %admin1cPwdLine% %sqluserLine% %sqlpasswLine% %fulldropLin%
+    "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe" -file drop_db.ps1 -server1c %server1c% -agentPort %agentPort% -serverSql %serverSql% -infobase %base% %admin1cUser% %admin1cPwd% %sqluserLine% %sqlpassw% %fulldrop%
 )
 
 rem 32-bit PowerShell 	C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe
